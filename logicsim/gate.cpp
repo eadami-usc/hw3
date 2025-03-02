b07e0ea6c4442b8aca96eab3e388e2b7e573b658
Evan Adami <eadami@usc.edu>
2025-03-02 13:21:54 -0800

    Finished logicsim

--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -7,11 +7,9 @@
 
 Gate::Gate(int num_inputs, Wire* output) 
 	: m_output(output), m_inputs(num_inputs), m_delay(0), m_current_state('X')
-{
-    
-}
+{}
 
-Gate::~Gate(){}
+Gate::~Gate() {}
 
 void Gate::wireInput(unsigned int id, Wire* in)
 {
@@ -87,4 +85,27 @@ Event* Or2Gate::update(uint64_t current_time)
          
 	}
   return e;
+}
+
+NotGate::NotGate(Wire* a, Wire* o) : Gate(1,o) {
+	wireInput(0,a);
+}
+
+Event* NotGate::update(uint64_t current_time) {
+	char state = '0';
+	Event* e = nullptr;
+	char in = m_inputs[0]->getState();
+	if (in == '0') {
+		state = '1';
+	} else if(in == '1') {
+		state = '0';
+	} else if(in == 'X') {
+		state = 'X';
+	}
+	if (state != m_current_state) {
+		m_current_state = state;
+		uint64_t next = current_time + m_delay;
+		e = new Event {next, m_output, state};
+	}
+	return e;
 }
